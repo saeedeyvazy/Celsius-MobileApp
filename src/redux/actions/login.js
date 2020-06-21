cb7ba17d267c45c0f578b472982f0331ac78ccc4
saeed eyvazy <saeed.eyvazy@gmail.com>
2020-06-21 21:36:57 +0430

    Add Login Call API

--- src/redux/actions/login.js
+++ src/redux/actions/login.js
@@ -1,1 +1,19 @@
-export const login = async () => {}
+import axios from 'axios'
+import { AsyncStorage } from 'react-native'
+import { loginUrl } from '../../utility/api'
+import { isNullOrEmpty } from '../../utility/string'
+export const login = async (username, pwd) => {
+	try {
+		const response = await axios.post(
+			`${loginUrl}?username=${username}&pwd=${pwd}`
+		)
+		await AsyncStorage.setItem(
+			'@login:accessToken',
+			response.data.Result.AccToken
+		)
+		if (isNullOrEmpty(response.data.Result.AccToken)) return false
+		return true
+	} catch (error) {
+		return false
+	}
+}
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,25 +1,50 @@
 import React, { useState } from 'react'
 import LoginScreen from 'react-native-login-screen'
+import { login } from '../../redux/actions/login'
+import AwesomeAlert from 'react-native-awesome-alerts'
 
 const Login = ({ navigation }) => {
 	const [username, setUsername] = useState(null)
+	const [pwd, setPwd] = useState(null)
 	const [switchValue, setSwitchValue] = useState(false)
+	const [showLoginFailedAlert, setShowLoginFailedAlert] = useState(false)
+
+	const onPressLogin = async () => {
+		const isLoginSuccessFully = await login(username, pwd)
+		if (isLoginSuccessFully) navigation.navigate('BottomNavigation')
+		else setShowLoginFailedAlert(true)
+	}
 
 	return (
-		<LoginScreen
-			disableSettings
-			switchTextStyle={{ color: 'black' }}
-			logoText='CelsiusPro'
-			loginButtonBackgroundColor='#282828'
-			source={require('../../img/wheat3.jpeg')}
-			switchValue={switchValue}
-			onPressLogin={() => navigation.navigate('BottomNavigation')}
-			usernameOnChangeText={(username) => setUsername(username)}
-			passwordOnChangeText={(password) => console.log('Password: ', password)}
-			onSwitchValueChange={(switchValue) => {
-				setSwitchValue(switchValue)
-			}}
-		></LoginScreen>
+		<>
+			<LoginScreen
+				disableSettings
+				switchTextStyle={{ color: 'black' }}
+				logoText='CelsiusPro'
+				loginButtonBackgroundColor='#282828'
+				source={require('../../img/wheat3.jpeg')}
+				switchValue={switchValue}
+				onPressLogin={async () => onPressLogin()}
+				usernameOnChangeText={(username) => setUsername(username)}
+				passwordOnChangeText={(password) => setPwd(password)}
+				onSwitchValueChange={(switchValue) => {
+					setSwitchValue(switchValue)
+				}}
+			></LoginScreen>
+			<AwesomeAlert
+				show={showLoginFailedAlert}
+				showProgress={false}
+				title='Login Failed'
+				message='Please check your username or password or internet connection'
+				closeOnTouchOutside={true}
+				closeOnHardwareBackPress={true}
+				showConfirmButton={true}
+				confirmText='      OK        '
+				confirmButtonColor='#DD6B55'
+				onConfirmPressed={() => setShowLoginFailedAlert(false)}
+				messageStyle={{ textAlign: 'center' }}
+			/>
+		</>
 	)
 }
 
